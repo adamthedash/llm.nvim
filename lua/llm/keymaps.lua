d82f5827262069fb8aa86ec60f0e3a159f13450d
adamthedash <adamthedash2@gmail.com>
2024-11-03 14:18:31 +0000

    disable default keymaps and make accept/reject functions public

--- lua/llm/keymaps.lua
+++ lua/llm/keymaps.lua
@@ -5,14 +5,14 @@ local M = {
   setup_done = false,
 }
 
-local function accept_suggestion()
+function M.accept_suggestion()
   if not completion.suggestion then
     return
   end
   vim.schedule(completion.complete)
 end
 
-local function dismiss_suggestion()
+function M.dismiss_suggestion()
   if not completion.suggestion then
     return
   end
@@ -30,13 +30,13 @@ function M.setup()
   local accept_keymap = config.get().accept_keymap
   local dismiss_keymap = config.get().dismiss_keymap
 
-  vim.keymap.set("i", accept_keymap, accept_suggestion, { expr = true })
-
-  vim.keymap.set("n", accept_keymap, accept_suggestion, { expr = true })
-
-  vim.keymap.set("i", dismiss_keymap, dismiss_suggestion, { expr = true })
-
-  vim.keymap.set("n", dismiss_keymap, dismiss_suggestion, { expr = true })
+  -- vim.keymap.set("i", accept_keymap, accept_suggestion, { expr = true })
+  --
+  -- vim.keymap.set("n", accept_keymap, accept_suggestion, { expr = true })
+  --
+  -- vim.keymap.set("i", dismiss_keymap, dismiss_suggestion, { expr = true })
+  --
+  -- vim.keymap.set("n", dismiss_keymap, dismiss_suggestion, { expr = true })
 
   M.setup_done = true
 end
